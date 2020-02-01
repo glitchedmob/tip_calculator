14b30c2eba46be1690ba4eb5eefdabcb9132e5d9
Levi Zitting <me@levizitting.com>
2020-01-31 22:54:47 -0600

    Add support for landscape mode

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -6,15 +6,44 @@ import 'package:tip_calculator/widgets/keypad.dart';
 class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.width);
+
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(child: ResultDisplay()),
-          Container(height: 3, color: Theme.of(context).primaryColor),
-          Padding(padding: EdgeInsets.only(bottom: 30)),
-          Keypad(),
-        ],
-      ),
+      body: MediaQuery.of(context).orientation == Orientation.portrait
+          ? buildPortrait(context)
+          : buildLandscape(context),
+    );
+  }
+
+  Widget buildPortrait(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(child: ResultDisplay()),
+        Container(height: 3, color: Theme.of(context).primaryColor),
+        Padding(padding: EdgeInsets.only(bottom: 30)),
+        Keypad(),
+      ],
+    );
+  }
+
+  Widget buildLandscape(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 3,
+          child: ResultDisplay(),
+        ),
+        Container(width: 3, color: Theme.of(context).primaryColor),
+        Expanded(
+          flex: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Keypad(),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
