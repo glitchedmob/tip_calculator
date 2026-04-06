20f2a83666927f08cefd36424e50279bccda4a5c
Levi Zitting <me@levizitting.com>
2026-04-05 22:47:18 -0500

    feat: upgrade android to latest template

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,20 +18,20 @@ if (localPropertiesFile.exists()) {
     }
 }
 
-val sentryDsn: String = localProperties.getProperty("sentry.dsn")
+val sentryDsn: String = localProperties.getProperty("sentry.dsn", "")
 
 android {
     namespace = "com.levizitting.tipcalculator"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -43,7 +43,6 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         manifestPlaceholders["sentryDsn"] = sentryDsn
-//        multidex = true
     }
 
     buildTypes {
